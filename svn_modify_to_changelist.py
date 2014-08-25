803a8b05dc0db7aae8876e49af2dd2c0361ae69a
Zhifeng Deng <zhdeng@linkedin.com>
2014-08-25 15:40:46 -0700

    whatever

---
+++ svn_modify_to_changelist.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+""" 
+Grasp all svn update without changelist to change list
+"""
+import sys
+import os
+import subprocess
+
+def main(argv):
+  if len(argv) < 2:
+    print '%s changelist' % argv[0]
+    return
+
+  ps = subprocess.Popen('svn st',
+      shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+  output = ps.communicate()[0]
+  lines = output.split('\n')
+
+  modifyFiles = []
+
+  for line in lines:
+    if line.startswith('--- Changelist'):
+      break
+    if line.startswith('M'):
+      modifyFiles.append(line.split('M')[-1].strip())
+
+  print 'Found modified files:---------------'
+  for line in modifyFiles:
+    print line
+  print 'List files end --------------------'
+
+  allFiles = ' '.join(modifyFiles) 
+
+  cmd = 'svn changelist %s %s' % (argv[1], allFiles)
+  print cmd
+  assert os.system(cmd) == 0
+
+if __name__ == '__main__':
+  main(sys.argv)
